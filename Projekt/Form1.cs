7fb1cf28aa6223472fda59708d9b6cfcaa9d21fb
mlomowski <mlomowski@gmail.com>
2022-10-26 22:52:30 +0200

    Test2

--- Projekt/Form1.cs
+++ Projekt/Form1.cs
@@ -19,7 +19,7 @@ namespace Projekt
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            //ruch o -10
+            //ruch
             move(-10);
           
         }
